287dea0c55c86978549f6f714e423f435b484dbc
minzuy <duybmse18042@fpt.edu.vn>
2024-09-29 23:31:03 +0700

    Layout template

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/logins/login";
 import RegisterPage from "./pages/registers/register";
-import UserManagementPage from "./pages/user-manages-page/UserManagement";
-import FishManagementPage from "./pages/fish-manages-page/FishMangement";
+import UserManagementPage from "./pages/admin-home/UserManagement";
+import FishManagementPage from "./pages/admin-home/FishMangement";
+import AdminHomePage from "./pages/admin-home/admin";
 function App() {
   const router = createBrowserRouter([
     {
@@ -24,6 +25,10 @@ function App() {
     {
       path: "fish-manage",
       element: <FishManagementPage />,
+    },
+    {
+      path: "admin",
+      element: <AdminHomePage />,
     },
   ]);
   return <RouterProvider router={router} />;
